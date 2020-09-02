e99b0c2f4459baaa1a3989cadebd3c2660c4b0e8
maurya20 <mukesh.ice17@gmail.com>
2020-09-02 11:32:45 +0530

    Initial footer componenet.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 export class App extends Component {
   render() {
     return (
       <div>
-        
+        <Header />
+        <Footer />
       </div>
     )
   }
