2562b9073da1daa59fc20bb9edcf553a0e62a943
maurya20 <mukesh.ice17@gmail.com>
2020-09-03 11:10:05 +0530

    Header changed toggle implemented logo added.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,34 @@
 import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import {NavDropdown, Nav, } from "react-bootstrap"
+import logo from "./images/logo.png"
 
 export class Header extends Component {
     render() {
         return (
-            <header>
-            <nav className='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
-              <a className='navbar-brand' href='/'>
-                Fixed navbar
-              </a>
-              <button
-                className='navbar-toggler'
-                type='button'
-                data-toggle='collapse'
-                data-target='#navbarCollapse'
-                aria-controls='navbarCollapse'
-                aria-expanded='false'
-                aria-label='Toggle navigation'>
-                <span className='navbar-toggler-icon' />
-              </button>
-              <div className='collapse navbar-collapse' id='navbarCollapse'>
-                <ul className='navbar-nav mr-auto'>
-                  <li className='nav-item active'>
-                    <a className='nav-link' href='/'>
-                      Home <span className='sr-only'>(current)</span>
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link' href='/'>
-                      Link
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link disabled' href='/' tabIndex={-1} aria-disabled='true'>
-                      Disabled
-                    </a>
-                  </li>
-                </ul>
-                <form className='form-inline mt-2 mt-md-0'>
-                  <input className='form-control mr-sm-2' type='text' placeholder='Search' aria-label='Search' />
-                  <button className='btn btn-outline-success my-2 my-sm-0' type='submit'>
-                    Search
-                  </button>
-                </form>
-              </div>
-            </nav>
-          </header>
+          <Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
+          <Navbar.Brand href="#home"><img style={{width:'150px', height:'30px'}}src={logo} alt="Logo"></img></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         )
     }
 }
