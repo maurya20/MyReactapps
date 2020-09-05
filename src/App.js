31c6aa443c2884cacdd1b48c29f69225817994c4
maurya20 <mukesh.ice17@gmail.com>
2020-09-05 12:02:21 +0530

    initial form using react

--- src/App.js
+++ src/App.js
@@ -23,11 +23,7 @@ class App extends Component {
             
           </Switch>
         </Router>
-       
-        <h3 className="container">
-        M & M Technologies </h3>
-        <p className="container">Company introduction Featured snippet from the web Simply put, your company profile is a professional introduction and aims to inform people (primarily prospective buyers and stakeholders) your products, services, and current status. A well written company profile is a great opportunity for your company to differentiate itself.</p>
-        
+      
         <Footer />
       </div>
     )
