a1f8b209288043b381fbd94565c619be8fc55ba6
maurya20 <mukesh.ice17@gmail.com>
2020-11-07 11:26:34 +0530

    Popup form and showing data in table task completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,123 +1,124 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import '../index.css'
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Modal from "./Modal";
-import {useState} from "react"
+import React, { useState } from "react";
 
 const Home = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [dimension, setDimension] = useState("");
 
-    const [modal, setModel] = useState(false)
-    const [name, setName] = useState("")
-    const [modalInputName, setmodalInputName] = useState("")
-    
-  
-    const handleChange = (e)=> {
-      const target = e.target;
-      const name = target.name;
-      const value = target.value;
-    }
-
-    const handleSubmit = (e) =>{
-        setName({ name: modalInputName });
-      
-    }
-  
-    const modalOpen = ()=>{
-      setModel({ modal: true });
-    }
-  
-    const modalClose = ()=> {
-      setModel({
-        modal: false
-      })
-    }
-  
-    return (
-        <div className="container">
-            <h3 style={{textAlign:"center",color:"red"}}>Home</h3>
-            <br></br>
-
-            <a href="javascript:;" onClick={e => modalOpen(e)}>
-          Open Modal
-        </a>
-        <Modal show={modal} handleClose={e => modalClose(e)}>
-       
-          <h2>Hello Modal</h2>
-          <div className="form-group">
-            <label>Enter Name:</label>
-            <input
-              type="text"
-              value={modalInputName}
-              name="modalInputName"
-              onChange={e => handleChange(e)}
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <button onClick={e =>handleSubmit(e)} type="button">
-              Save
-            </button>
-          </div>
-        </Modal>
-
-            <h3>A table to show the above data with a choice of edit and delete</h3>
-            <br></br>
-            <br></br>
-
-
-
-
-            {/* <div>
-          <table className="table table-sm" style={{ border: "1px solid" }}>
+  const table = () => {
+    if (name === "" && price === "") {
+      return null;
+    } else {
+      return (
+        <div>
+          <h4 style={{ color: "red", textAlign: "center" }}>Product table</h4>
+          <table className="table table-hover">
             <thead>
               <tr>
-                <th>Compaign</th>
-                <th>Status</th>
-                <th style={{ paddingLeft: "40px" }}>Opens</th>
-                <th>Clicks</th>
-                <th style={{ width: "20%", paddingLeft: "70px" }}>Actions</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Dimension(l*b*h)</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              
-                <tr>
-                  <td style={{ width: "50%" }}>
-                    <Link>{item.campaignName}</Link>
-                  </td>
-                  <td>
-                    <span className="badge badge-pill badge-success btn-block">
-                     
-                    </span>
-                  </td>
-                  <td style={{ width: "20%", paddingLeft: "40px" }}>
-                   
-                  </td>
-                  <td></td>
-                  <td style={{ width: "20%", paddingLeft: "70px" }}>
-                    <li class="list-inline-item">
-                      <button
-                        class="btn btn-warning"
-                        type="button"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Edit"
-                      >
-                        <i class="fa fa-edit"></i>
-                      </button>
-                    </li>
-                    <button className="btn btn-danger">
-                      <i className="fa fa-trash"></i>
+              <tr>
+                <td>{name}</td>
+                <td>{description}</td>
+                <td>{price}</td>
+                <td>{dimension}</td>
+
+                <td>
+                  <li class="list-inline-item">
+                    <button
+                      class="btn btn-warning"
+                      type="button"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Edit"
+                    >
+                      <i class="fa fa-edit"></i>
                     </button>
-                  </td>
-                </tr>
-              ))}
+                  </li>
+                  <button className="btn btn-danger">
+                    <i className="fa fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
             </tbody>
           </table>
-        </div> */}
-
         </div>
-    )
-}
+      );
+    }
+  };
+  return (
+    <div className="container">
+      <h3 style={{ color: "red", textAlign: "center" }}>Home page </h3>
+      <button
+        type="button"
+        className="btn btn-info btn-lg"
+        data-toggle="modal"
+        data-target="#myModal"
+      >
+        Add Product
+      </button>
+      <div className="modal fade" id="myModal" role="dialog">
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal">
+                &times;
+              </button>
+              <h4 className="modal-title"></h4>
+            </div>
+            <div className="modal-body">
+              <label className="mb-2 mr-sm-2">Name:</label>
+              <input
+                type="text"
+                className="form-control mb-2 mr-sm-2"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <label className="mb-2 mr-sm-2">Description:</label>
+              <input
+                type="text"
+                className="form-control mb-2 mr-sm-2"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <label className="mb-2 mr-sm-2">Price:</label>
+              <input
+                type="text"
+                className="form-control mb-2 mr-sm-2"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <label className="mb-2 mr-sm-2">Dimension(l*b*h):</label>
+              <input
+                type="text"
+                className="form-control mb-2 mr-sm-2"
+                onChange={(e) => setDimension(e.target.value)}
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-default"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      {/* For Table  */}
+      {table()}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
